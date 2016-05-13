3da45ea029a88dd1d9ca7516d5e266f83106e697
Dimon <Dimon@ScieteX.Dlink>
2016-05-13 22:32:07 +0300

    Ver. 1.0

--- ZEVS_Project/src/zevs/workspace/Workspace.java
+++ ZEVS_Project/src/zevs/workspace/Workspace.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 
 import zevs.ConnectionDB;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.JTabbedPane;
@@ -23,6 +24,10 @@ import javax.swing.JScrollPane;
 import javax.swing.JButton;
 
 
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
+import javax.swing.text.Highlighter.HighlightPainter;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -33,6 +38,9 @@ public class Workspace extends ConnectionDB {
     public 	JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
     private JTextField textField;
 	private Connection connectionUser;
+	JTextArea textArea;
+	private Highlighter highlighter;
+	private Highlighter.HighlightPainter painter;
 	//private Connection connectionAdmin;
 	public Workspace() {
 		initialize();
@@ -44,6 +52,8 @@ public class Workspace extends ConnectionDB {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		highlighter = new DefaultHighlighter();
+		painter = new DefaultHighlighter.DefaultHighlightPainter(Color.YELLOW);
 		frame = new JFrame();
 		frame.setBounds(100, 100, 652, 395);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -59,18 +69,25 @@ public class Workspace extends ConnectionDB {
 		panel.add(label, "flowx,cell 0 0,alignx left,aligny center");
 		
 		textField = new JTextField();
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				search();
+			}
+		});
 		textField.setToolTipText("\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0442\u0435\u043A\u0441\u0442 \u0434\u043B\u044F \u043F\u043E\u0438\u0441\u043A\u0430");
 		panel.add(textField, "cell 0 0,growx,aligny top");
 		textField.setColumns(10);
+		//textField.setHighlighter(highlighter);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		panel.add(scrollPane, "cell 0 1,grow");
 		
-		final JTextArea textArea = new JTextArea();
+		textArea = new JTextArea();
 		textArea.setAutoscrolls(false);
 		textArea.setEditable(false);
 		scrollPane.setViewportView(textArea);
 		textArea.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+		textArea.setHighlighter(highlighter);
 		JButton btnNewButton = new JButton("<<<");
 		panel.add(btnNewButton, "cell 0 0");
 		
@@ -116,4 +133,36 @@ public class Workspace extends ConnectionDB {
        }
 		
 	}
-}
+	protected void search()
+	{
+		try
+		{
+		highlighter.removeAllHighlights();
+		String textA = textArea.getText().toLowerCase();
+		String textF = textField.getText().toLowerCase();
+		int start = textA.indexOf(textF,0);
+		int end = start + textF.length();	
+		while(true)
+		{
+			if(start > textA.length())
+			{
+				break;
+			}
+			else
+			{
+				int test = start;
+				start = textA.indexOf(textF, test);
+				end = start + textF.length();
+				 highlighter.addHighlight(start, end, painter);
+			}
+		}
+	     //highlighter.addHighlight(start, end, painter);
+	     //textArea.setCaretPosition(start);
+	            }
+	            catch (Exception e)
+	            {
+	            	e.printStackTrace();
+	            }
+		
+	 }
+	}
